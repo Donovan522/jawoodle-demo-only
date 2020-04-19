b581d65354f26d5991a99413a2441b4cdb73b613
Donovan C. Young <dyoung522@gmail.com>
2020-04-19 19:58:36 -0400

    add additional zombie definitions

--- script/mkentityclasses.sh
+++ script/mkentityclasses.sh
@@ -1,11 +1,13 @@
 # Remove entries
 for ZOMBIE in $(cat zombie_names); do
-  echo "<remove xpath=\"/entity_classes/entity_class[@name='${ZOMBIE}']\" />"
+  echo "  <remove xpath=\"/entity_classes/entity_class[@name='${ZOMBIE}']\" />"
 done
 
+echo
+
 # Add entries
-echo "<insertAfter xpath=\"/entity_classes/entity_class[@name='zombieTemplateDemo']\">"
+echo "  <insertAfter xpath=\"/entity_classes/entity_class[@name='zombieTemplateDemo']\">"
 for ZOMBIE in $(cat zombie_names); do
   echo "    <entity_class name=\"${ZOMBIE}\" extends=\"zombieTemplateDemo\" />"
 done
-echo "</insertAfter>"
+echo "  </insertAfter>"
